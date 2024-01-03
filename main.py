6d3bb877e615972f4c9dd2b46ef066011170e8ae
bkenks <bkenks@gmail.com>
2024-01-02 23:33:51 -0500

    Project completed

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def GetVectorStore(textChunks):
 
 def GetConversationChain(vectorStore):
     llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key="chatHistory", return_messages=True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversationChain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorStore.as_retriever(),
@@ -52,8 +52,14 @@ def GetConversationChain(vectorStore):
 
 
 def HandleUserInput(userQuestion):
-    response = st.session_state.conversation({"question":userQuestion})
-    st.write(response)
+    response = st.session_state.conversation({"question": userQuestion})
+    st.session_state.chat_history = response["chat_history"]
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
@@ -64,14 +70,13 @@ def main():
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
     st.header("Chat with multiple PDFs :books:")
     userQuestion = st.text_input("Ask a question about your documents:")
     if userQuestion:
         HandleUserInput(userQuestion)
-
-    st.write(user_template.replace("{{MSG}}", "Hello Bot"), unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}", "Hello Human"), unsafe_allow_html=True)
 
     with st.sidebar:
         st.subheader("Your documents")
